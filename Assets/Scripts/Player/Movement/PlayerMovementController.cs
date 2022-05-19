b1eb94225c884fbcdc5f91483e5e8c1ffbdae40f
DaanDuits <daand04@gmail.com>
2022-05-19 09:14:27 +0200

    kleine movement changes

--- Assets/Scripts/Player/Movement/PlayerMovementController.cs
+++ Assets/Scripts/Player/Movement/PlayerMovementController.cs
@@ -4,33 +4,42 @@ using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviour
 {
+    Rigidbody2D rb;
+
+    //movement speed
     public int jumpheight;
     public float speed;
 
     public float distance;
 
-
-    Rigidbody2D rb;
-
+    //ground checking
     public Transform groundCheck;
     public bool isGrounded;
+
     public LayerMask groundMask;
 
+    //wall jumping
     public Transform[] wallCheck;
-    public bool onWallright;
+    public bool onWallRight;
     public bool onWallLeft;
     public bool onWall;
+
+    public bool wallJump;
+
     public LayerMask wallMask;
     public float wallGravity;
 
     // Start is called before the first frame update
     void Start()
     {
+        //get the players rigidbody2D
          rb = this.GetComponent<Rigidbody2D>();
     }
 
+    //on collision with something
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        //if it collides with a wall reset velocity
         if(collision.gameObject.tag == "Wall")
         {
             rb.velocity = new Vector2(0, 0);
@@ -43,9 +52,9 @@ public class PlayerMovementController : MonoBehaviour
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.5f, groundMask);
 
         //check if on a wall
-        onWallright = Physics2D.OverlapCircle(wallCheck[0].position, 0.1f, wallMask);
+        onWallRight = Physics2D.OverlapCircle(wallCheck[0].position, 0.1f, wallMask);
         onWallLeft = Physics2D.OverlapCircle(wallCheck[1].position, 0.1f, wallMask);
-        if (onWallright || onWallLeft)
+        if (onWallRight || onWallLeft)
         {
             onWall = true;
         }
@@ -54,20 +63,43 @@ public class PlayerMovementController : MonoBehaviour
             onWall = false;
         }
 
+        if (isGrounded)
+        {
+            wallJump = false;
+        }
+
         //Movement
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            this.transform.position -= this.transform.right * speed * Time.deltaTime;
+            if (wallJump)
+            {
+                rb.velocity = new Vector2(0, 0);
+                wallJump = false;
+            }
+            if(!onWallLeft)
+            {
+                float dirX = Input.GetAxis("Horizontal");
+                rb.velocity = new Vector2(dirX * speed, rb.velocity.y);
+            }
         }
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            this.transform.position += this.transform.right * speed * Time.deltaTime;
+            if (wallJump)
+            {
+                rb.velocity = new Vector2(0, 0);
+                wallJump = false;
+            }
+            if (!onWallRight)
+            {
+                float dirX = Input.GetAxis("Horizontal");
+                rb.velocity = new Vector2(dirX * speed, rb.velocity.y);
+            }
         }
 
         //Jump
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            rb.velocity = new Vector2(0, jumpheight);
+            rb.velocity = new Vector2(rb.velocity.x, jumpheight);
         }
 
         //walljump
@@ -75,12 +107,14 @@ public class PlayerMovementController : MonoBehaviour
         {
             rb.gravityScale = wallGravity;
 
-            if(Input.GetKeyDown(KeyCode.Space) && onWallright)
+            if(Input.GetKeyDown(KeyCode.Space) && onWallRight)
             {
+                wallJump = true;
                 rb.velocity = new Vector2(-distance, distance);
             }
             if (Input.GetKeyDown(KeyCode.Space) && onWallLeft)
             {
+                wallJump = true;
                 rb.velocity = new Vector2(distance, distance);
             }
         }
